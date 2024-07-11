8e5c1f50fc51b40b9a63b1e60229de7a8390fc8a
MasrulSakib <Masrul.Sakib69@gmail.com>
2024-07-11 15:22:13 +0600

    Dashboard layout changed

--- src/DashBoardLayout/DashboardLayout.js
+++ src/DashBoardLayout/DashboardLayout.js
@@ -5,44 +5,66 @@ import useBuyer from '../Hooks/useBuyer';
 import { AuthContext } from '../UserValidation/Context/AuthProvider';
 import useAdmin from '../Hooks/useAdmin';
 import useSeller from '../Hooks/useSeller';
+import { RiMenuUnfold3Line } from "react-icons/ri";
 
 const DashboardLayout = () => {
-    const { user } = useContext(AuthContext)
-    const [isBuyer] = useBuyer(user?.email)
-    const [isAdmin] = useAdmin(user?.email)
-    const [isSeller] = useSeller(user?.email)
+    const { user } = useContext(AuthContext);
+    const [isBuyer] = useBuyer(user?.email);
+    const [isAdmin] = useAdmin(user?.email);
+    const [isSeller] = useSeller(user?.email);
 
     return (
         <div className='min-h-screen'>
             <Navbar></Navbar>
             <div className="drawer lg:drawer-open">
-                <input id="dashborad-drawer" type="checkbox" className="drawer-toggle" />
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
+                    <div className="flex m-4">
+                        <label
+                            htmlFor="dashboard-drawer"
+                            role="button"
+                            className="btn btn-sm btn-outline rounded-lg btn-error drawer-button lg:hidden"
+                        >
+                            Menu
+                            <RiMenuUnfold3Line className='text-xl' />
+                        </label>
+                    </div>
                     <Outlet></Outlet>
                 </div>
                 <div className="drawer-side" style={{ top: 'auto' }}>
                     <label htmlFor="dashboard-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content z-30">
-                        {
-                            isBuyer &&
-                            <li><Link to={'/dashboard/myorders'}>My Orders</Link></li>
-                        }
-                        {
-                            isSeller &&
+                        <li className="lg:hidden flex items-end mb-4">
+                            <label htmlFor="dashboard-drawer" role='button' className="btn btn-sm btn-circle btn-outline">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </label>
+                        </li>
+                        {isBuyer && <li><Link to={'/dashboard/myorders'}>My Orders</Link></li>}
+                        {isSeller && (
                             <>
                                 <li><Link to={'/dashboard/addproduct'}>Add a Product</Link></li>
                                 <li><Link to={'/dashboard/myproducts'}>My Products</Link></li>
                             </>
-                        }
-                        {
-                            isAdmin &&
+                        )}
+                        {isAdmin && (
                             <>
                                 <li><Link to={'/dashboard/allusers'}>All Users</Link></li>
                                 <li><Link to={'/dashboard/users/buyers'}>All Buyers</Link></li>
                                 <li><Link to={'/dashboard/users/sellers'}>All Sellers</Link></li>
                                 <li><Link to={'/dashboard/reportedcars/post'}>Reported Post</Link></li>
                             </>
-                        }
+                        )}
                     </ul>
                 </div>
             </div>
--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -34,13 +34,15 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-100 border-b-2 border-red-500">
             <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                <div className=" lg:hidden ">
+                    <div className='dropdown'>
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={1} className="menu menu-sm dropdown-content mt-3 z-30 p-2 shadow bg-error rounded-box w-52">
+                            {menuItems}
+                        </ul>
                     </div>
-                    <ul tabIndex={1} className="menu menu-sm dropdown-content mt-3 z-30 p-2 shadow bg-error rounded-box w-52">
-                        {menuItems}
-                    </ul>
                 </div>
                 <Link to={'/'} className="btn btn-ghost md:text-2xl text-xl font-semibold"><button>Re<span className='text-error'>Car</span>Nation</button></Link>
             </div>
@@ -49,11 +51,7 @@ const Navbar = () => {
                     {menuItems}
                 </ul>
             </div>
-            <div className='navbar-end lg:hidden'>
-                <label htmlFor="dashborad-drawer" tabIndex={2} role="button" className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-            </div>
+
         </div>
     );
 };
